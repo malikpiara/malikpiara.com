aa0bf8437c2658803e565abf4030eb4a1957e1cf
Malik Piara <malikpiara@gmail.com>
2020-12-09 00:13:01 +0000

    Added read more function and contact page

--- main.js
+++ main.js
@@ -1,4 +1,32 @@
 document.querySelector('#dark').onclick = function() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+    document.body.classList.toggle("dark-mode");
+}
+
+
+var coll = document.getElementsByClassName("show-more");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+
+document.querySelector('#disappear').onclick = function() {
+  var element = document.body;
+  element.classList.toggle("disappear");
+}
+
+// newsletter
+
+function getEmail() {
+  let email = document.querySelector('#email').value;
+  alert('Hello, ' + email);
 }
